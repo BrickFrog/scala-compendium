bb58a0c374744c906ff061360e410d4e5facda89
Justin <brickfrog@users.noreply.github.com>
2020-01-12 21:54:10 -0500

    Initial

---
+++ functional-programming-in-scala/src/main/scala/Main.scala
@@ -0,0 +1,18 @@
+//* Hello! *//
+
+object Chapter02 {
+
+  // Exercise 2.1
+  def fib(n: Int): Int = {
+      @annotation.tailrec
+      def go(n: Int, a: Int, b: Int): Int =
+          // base case
+          if (n == 0) a
+          else go(n-1, b, a + b)
+      go(n, 0, 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+      println(fib(0))
+  }
+}
